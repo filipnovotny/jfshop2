89253cdcc18ec3b5e886222742fbfea630ac4528
Novotny <filipnovo@gmail.com>
2016-09-25 17:19:31 +0200

    deploy scripts again

--- update.sh
+++ update.sh
@@ -7,6 +7,19 @@ cp -rf deploy/local_settings.py.template local_settings.py
 cp -rf deploy/gunicorn.conf.py.template ./gunicorn.conf.py
 
 pip3 install -r requirements.txt
+
+if [ -f password.txt ];
+then
+   cp -rf deploy/nginx.conf.template /usr/local/nginx/conf/nginx.conf
+else
+    echo "please create a password.txt file here with your mysql password"
+    exit
+fi
+password=`cat password.txt`
+sed -i -- "s/<%PASSWORD%>/$password/g" local_settings.py
+echo "create database `jfshop2`"mysql -uroot -p$password
+
+
 python manage.py createdb --noinput
 python manage.py migrate
 
--- update_root.sh
+++ update_root.sh
@@ -1,13 +1,3 @@
-if [ -f password.txt ];
-then
-   cp -rf deploy/nginx.conf.template /usr/local/nginx/conf/nginx.conf
-else
-    echo "please create a password.txt file here with your mysql password"
-    exit
-fi
-password=`cat password.txt`
-sed -i -- "s/<%PASSWORD%>/$password/g" local_settings.py
-mysql -uroot -p$password -e "create database 'jfshop2'"
 
 if [ -f /usr/local/nginx/conf/nginx.conf ];
 then
