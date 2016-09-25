8f47173d3a8a02ca865a1ec8aa162efa23b58c82
Novotny <filipnovo@gmail.com>
2016-09-25 15:22:03 +0200

    adapted install for python3

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-apt-get --assume-yes install  build-essential mysql-server python3-dev python-virtualenv libpng-dev zlib1g-dev libmysqlclient-dev python-mysqldb nginx git
-git clone git@github.com:filipnovotny/jfshop2.git
-pip install --upgrade virtualenv
+apt-get --assume-yes install  build-essential mysql-server python3-dev python-virtualenv libpng-dev libjpeg-dev zlib1g-dev libmysqlclient-dev python-mysqldb nginx git python3-pip
+#git clone git@github.com:filipnovotny/jfshop2.git
+pip3 install --upgrade virtualenv
 virtualenv ../jfshop2-env
 source ../jfshop2-env/bin/activate
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 ./update.sh
