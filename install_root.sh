6a3caaa33f725a8f65d8145e080d2505f8a7141d
Novotny <filipnovo@gmail.com>
2016-09-26 13:45:30 +0200

    added apache mod configuration

--- install_root.sh
+++ install_root.sh
@@ -1,1 +1,5 @@
 apt-get --assume-yes install  build-essential mysql-server python3-dev python-virtualenv libpng-dev libjpeg-dev zlib1g-dev libmysqlclient-dev python-mysqldb nginx git python3-pip runit
+sudo a2enmod proxy
+sudo a2enmod proxy_balancer
+sudo a2enmod proxy_http
+sudo service apache2 restart
