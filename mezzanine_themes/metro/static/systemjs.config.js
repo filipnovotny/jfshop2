aac06a45d7d6eb590ec09d06e12803c314e93968
Novotny <filipnovo@gmail.com>
2016-10-08 22:25:19 +0200

    typings and systemjs includes

--- mezzanine_themes/metro/static/systemjs.config.js
+++ mezzanine_themes/metro/static/systemjs.config.js
@@ -28,6 +28,7 @@
       // other libraries
       'rxjs':                      'npm:rxjs',
       'angular-in-memory-web-api': 'npm:angular-in-memory-web-api',
+      'underscore':                'npm:underscore',
 
       'angular2-modal': 'npm:angular2-modal',
       'angular2-modal/plugins/bootstrap' : 'npm:angular2-modal/bundles',
@@ -47,6 +48,10 @@
         main: './index.js',
         defaultExtension: 'js'
       },
+      'underscore':{
+        main: './underscore.js', 
+        defaultExtension: 'js'
+       },
       'angular2-modal': { 
         defaultExtension: 'js',
         main: 'bundles/angular2-modal.umd'
@@ -58,6 +63,14 @@
       'portfolio': { 
         defaultExtension: 'js'
       }
+    },
+    meta: {
+      'node_modules/exif-js/exif.js': {
+        exports: 'EXIF',
+        format: 'global'
+      }
     }
   });
 })(this);
+
+System.import('node_modules/exif-js/exif.js');
